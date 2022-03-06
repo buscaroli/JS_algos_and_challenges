91937a39f5ee3b9f777ba45b7341db75b9960f4f
mabus <m.buscaroli@outlook.com>
2022-03-06 12:58:17 +0000

    added three more challenges from Codewars

--- test.js
+++ test.js
@@ -2,17 +2,16 @@
 This file is use to try small scripts while coding the real algorithms/challenges.
 */
 
-const str = "hitherehello";
-const lastIndex = str.length - 1;
-console.log(str[lastIndex]);
-let newArr = [];
-for (let i = lastIndex; i >= 0; i--) {
-  newArr.push(str[i]);
-}
-console.log(newArr.join(""));
+let arr1 = [1, 2, 3]
+let arr2 = [4, 5, 6]
 
-newArr = [];
-for (let i = 0; i <= lastIndex; i++) {
-  newArr.unshift(str[i]);
+console.log(arr1.concat(arr2))
+
+const range = (start, stop, step) => {
+  return Array.from(
+    { length: (stop - start) / step + 1 },
+    (_, i) => start + i * step
+  )
 }
-console.log(newArr.join(""));
+
+console.log(range(5, 18, 3))
